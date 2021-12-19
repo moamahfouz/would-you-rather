514be3311d389417827644d3cd8da2d7499a6b72
mohamed <moa.mahfouz@gmail.com>
2021-12-19 22:13:48 +0200

    init app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class App extends Component {
+  componentDidMount() {
+    console.log("init app");
+  }
+  render() {
+    return <h2>Init app</h2>;
+  }
+}
+
+export default App;
